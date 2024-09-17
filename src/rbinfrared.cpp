8504954863d3915f63ca846f9b2f68724da4ba2a
Alexander ulbrich <alex.ulbrich.ulm@gmail.com>
2024-09-17 23:10:58 +0200

    first working version tested with pin 52 and 53

--- src/rbinfrared.cpp
+++ src/rbinfrared.cpp
@@ -3,27 +3,75 @@
 
 
 #define PIN_HANDLER(pin) \
-const int ir_pin_##pin = pin;\
 void pin_handler_##pin (void) \
 { \
   RBInfrared::handleInterrupt(pin); \
 }
 
-#define G_MAX_NUMBER_OF_IR_SENSORS 2
+#define G_MAX_NUMBER_OF_IR_SENSORS 17
 
-PIN_HANDLER (0)
-PIN_HANDLER (1)
+PIN_HANDLER (14)
+PIN_HANDLER (15)
+PIN_HANDLER (16)
+PIN_HANDLER (17)
+PIN_HANDLER (18)
+PIN_HANDLER (19)
+PIN_HANDLER (20)
+PIN_HANDLER (21)
+PIN_HANDLER (22)
+PIN_HANDLER (46)
+PIN_HANDLER (47)
+PIN_HANDLER (48)
+PIN_HANDLER (49)
+PIN_HANDLER (50)
+PIN_HANDLER (51)
+PIN_HANDLER (52)
+PIN_HANDLER (53)
 
 
 const int g_pin_list[] = { 
-    0, 
-    1
+    14, 
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    46,
+    47,
+    48,
+    49,
+    50,
+    51,
+    52,
+    53
 };
 
 void (* const g_pin_handler_table[])(void)  = {
-    pin_handler_0,
-    pin_handler_1,
+    pin_handler_14,
+    pin_handler_15,
+    pin_handler_16,
+    pin_handler_17,
+    pin_handler_18,
+    pin_handler_19,
+    pin_handler_20,
+    pin_handler_21,
+    pin_handler_22,
+    pin_handler_46,
+    pin_handler_47,
+    pin_handler_48,
+    pin_handler_49,
+    pin_handler_50,
+    pin_handler_51,
+    pin_handler_52,
+    pin_handler_53,
 };
+
+int RBInfrared::_pin_list[MAX_NUMBER_OF_IR_SENSORS];
+int RBInfrared::_timestamps[MAX_NUMBER_OF_IR_SENSORS];
+int RBInfrared::_values[MAX_NUMBER_OF_IR_SENSORS];
 
 int RBInfrared::getIndexByPin(int pin){
     for(int i = 0; i < MAX_NUMBER_OF_IR_SENSORS; i++)
@@ -53,6 +101,11 @@ void RBInfrared::init()
         {
             int pin = _pin_list[i];
             int index = getGlobalIndexByPin(pin);
+            if(index == -1)
+            {
+                Serial.println("ERROR: unsupported pin defined for rbinfrared");
+                continue;
+            }
             pinMode(pin, INPUT_PULLUP);
             attachInterrupt(pin, g_pin_handler_table[index], CHANGE);
             _values[i] = 0;
@@ -68,17 +121,25 @@ void RBInfrared::init()
 void RBInfrared::handleInterrupt(int pin)
 {
    int index = getIndexByPin(pin);
+   if(index == -1)
+   {
+        return;
+   }
    if(digitalRead(pin)) //rising
    {
-        _timestamps[index] = millis();
+        _timestamps[index] = micros();
    }
    else //falling
    {
-        _values[index] = millis() - _timestamps[index];
+        _values[index] = micros() - _timestamps[index];
+        if(_values[index] > 1000)
+            _values[index] = 1000;
    }
 }
 
 int RBInfrared::getValue(int index)
 {
-    return _values[index];
+    int tmp = _values[index];
+    _values[index] = 0;
+    return tmp;
 }
--- src/rbinfrared.h
+++ src/rbinfrared.h
@@ -15,28 +15,54 @@
 #define NOT_CONNECTED -1
 
 class RBInfrared{
-    private:
-    static int _pin_list[MAX_NUMBER_OF_IR_SENSORS];
-    static int _timestamps[MAX_NUMBER_OF_IR_SENSORS];
-    static int _values[MAX_NUMBER_OF_IR_SENSORS];
-
-    static int getIndexByPin(int pin);
-    static int getGlobalIndexByPin(int pin);
-
     public:
-    static void handleInterrupt(int pin);
+        RBInfrared(
+            int s0=NOT_CONNECTED,
+            int s1=NOT_CONNECTED,
+            int s2=NOT_CONNECTED,
+            int s3=NOT_CONNECTED,
+            int s4=NOT_CONNECTED,
+            int s5=NOT_CONNECTED,
+            int s6=NOT_CONNECTED,
+            int s7=NOT_CONNECTED,
+            int s8=NOT_CONNECTED,
+            int s9=NOT_CONNECTED,
+            int s10=NOT_CONNECTED,
+            int s11=NOT_CONNECTED,
+            int s12=NOT_CONNECTED,
+            int s13=NOT_CONNECTED,
+            int s14=NOT_CONNECTED,
+            int s15=NOT_CONNECTED)
+            {
+                RBInfrared::_pin_list[0] = s0;
+                RBInfrared::_pin_list[1] = s1;
+                RBInfrared::_pin_list[2] = s2;
+                RBInfrared::_pin_list[3] = s3;
+                RBInfrared::_pin_list[4] = s4;
+                RBInfrared::_pin_list[5] = s5;
+                RBInfrared::_pin_list[6] = s6;
+                RBInfrared::_pin_list[7] = s7;
+                RBInfrared::_pin_list[8] = s8;
+                RBInfrared::_pin_list[9] = s9;
+                RBInfrared::_pin_list[10] = s10;
+                RBInfrared::_pin_list[11] = s11;
+                RBInfrared::_pin_list[12] = s12;
+                RBInfrared::_pin_list[13] = s13;
+                RBInfrared::_pin_list[14] = s14;
+                RBInfrared::_pin_list[15] = s15;
+            };
+        void init();
+        int getValue(int index);
 
-    RBInfrared(
-        int s0=NOT_CONNECTED,
-        int s1=NOT_CONNECTED,
-        int s2=NOT_CONNECTED)
-        {
-            _pin_list[0] = s0;
-            _pin_list[1] = s1;
-            _pin_list[2] = s2;
-        };
-    void init();
-    int getValue(int index);
+        static void handleInterrupt(int pin);
+
+    private:
+        static int _pin_list[MAX_NUMBER_OF_IR_SENSORS];
+        static int _timestamps[MAX_NUMBER_OF_IR_SENSORS];
+        static int _values[MAX_NUMBER_OF_IR_SENSORS];
+
+        static int getIndexByPin(int pin);
+        static int getGlobalIndexByPin(int pin);
 };
 
 #endif
